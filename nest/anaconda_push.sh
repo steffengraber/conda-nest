3e7e0eff7fa392cc4a0771fc71dbb0208e6b278a
Steffen Graber <s.graber@fz-juelich.de>
2019-02-27 11:45:17 +0100

    add label nest.simulator

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -3,4 +3,4 @@
 VERSION="master"
 
 PKG_NAME=$(conda build . --output)
-anaconda -t $c_token upload -v $VERSION --force -u $c_user -l daily -l main $PKG_NAME
+anaconda -t $c_token upload -v $VERSION --force -u $c_user -l nest.simulator -l daily -l main $PKG_NAME
