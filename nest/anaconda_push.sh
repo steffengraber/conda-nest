044009d2ce88362ecd71005301151e9cbfb04746
Steffen Graber <s.graber@fz-juelich.de>
2019-02-28 15:23:49 +0100

    disable convert to macosx

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -7,6 +7,5 @@ PKG_NAME=$(conda build . --output)
 anaconda -t $c_token upload -v $VERSION --force -u $c_user -l release -l main $PKG_NAME
 
 # Convert to osx
-conda-convert -p osx-64 --dependencies clang_osx-64 --dependencies clangxx_osx-64 -f $PKG_NAME
-
-anaconda -t $c_token upload -v osx-64 --force -u $c_user -l release -l test $PKG_NAME
+# conda-convert -p osx-64 --dependencies clang_osx-64 --dependencies clangxx_osx-64 -f $PKG_NAME
+# anaconda -t $c_token upload -v osx-64 --force -u $c_user -l release -l test $PKG_NAME
