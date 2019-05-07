e923bd4132b957f8cfd9c34b8cacf5c5f8757bb8
Steffen Graber <s.graber@fz-juelich.de>
2019-05-07 12:02:24 +0200

    like conda-forge feedstock

--- nest/build.sh
+++ nest/build.sh
@@ -12,47 +12,21 @@ export LDFLAGS="-L$PREFIX/lib"
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-      -Dwith-mpi=ON \
-      -Dwith-openmp=ON \
+      -Dwith-mpi=OFF\
+      -Dwith-openmp=OFF \
       -Dwith-python=3 \
       -Dwith-gsl=$PREFIX \
       -DREADLINE_ROOT_DIR=$PREFIX \
       -DLTDL_ROOT_DIR=$PREFIX \
       ..
-make -j2
+
+make -j${CPU_COUNT}
 make install
-# this is needed to make the python bindings work
+
 cp $PREFIX/lib64/* $PREFIX/lib -r
 
-# setting variables for NEST in environment
-mkdir -p $PREFIX/etc/conda/activate.d
-mkdir -p $PREFIX/etc/conda/deactivate.d
-
-
-NEST_INSTALL_DIR=$PREFIX
-NEST_DATA_DIR="$NEST_INSTALL_DIR/share/nest"
-NEST_DOC_DIR="$NEST_INSTALL_DIR/share/doc/nest"
-NEST_MODULE_PATH="$NEST_INSTALL_DIR/lib/nest"
-NEST_PYTHON_PREFIX="$NEST_INSTALL_DIR/lib/python3.6/site-packages"
-PYTHONPATH="$NEST_PYTHON_PREFIX:$PYTHONPATH"
-PATH="$NEST_INSTALL_DIR/bin:$PATH"
-
-cat << EOF > $PREFIX/etc/conda/activate.d/activate-nest.sh
-#!/bin/bash
-export NEST_INSTALL_DIR=$PREFIX
-export NEST_DATA_DIR=$NEST_DATA_DIR
-export NEST_DOC_DIR=$NEST_DOC_DIR
-export NEST_MODULE_PATH=$NEST_MODULE_PATH
-export NEST_PYTHON_PREFIX=$NEST_PYTHON_PREFIX
-export PYTHONPATH=$PYTHONPATH
-export PATH=$PATH
-EOF
-
-cat << EOF > $PREFIX/etc/conda/deactivate.d/deactivate-nest.sh
-#!/bin/sh
-unset NEST_INSTALL_DIR
-unset NEST_DATA_DIR
-unset NEST_DOC_DIR
-unset NEST_MODULE_PATH
-unset NEST_PYTHON_PREFIX
-EOF
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    sed "s#!!!SP_DIR!!!#${SP_DIR}#g" "${RECIPE_DIR}/${CHANGE}.sh" > "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
