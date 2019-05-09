509e63a78668ca29b89f74726cec63e1e13f1da0
Steffen Graber <s.graber@fz-juelich.de>
2019-05-09 08:46:17 +0200

    osx try to use brew installed clang

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="v2.16.0"
+VERSION="2.16.0"
 conda install -y -c anaconda anaconda-client
 PKG_NAME=$(conda build ${HOME}/nest . --output)
 anaconda -t $c_token upload -v $VERSION --force -u $c_user -l daily -l main $PKG_NAME
