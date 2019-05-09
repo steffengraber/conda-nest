9ea787e2a57794d223411b2f4c620fd7ed40938e
Steffen Graber <s.graber@fz-juelich.de>
2019-05-09 08:42:54 +0200

    osx try to use brew installed clang

--- nest/build.sh
+++ nest/build.sh
@@ -2,23 +2,49 @@
 
 export MPI_FLAGS=--allow-run-as-root
 
-if [ $(uname) == Linux ]; then
+if [[ $(uname) == Linux ]]; then
     export MPI_FLAGS="$MPI_FLAGS;-mca;plm;isolated"
+	export CFLAGS="-I$PREFIX/include"
+	export LDFLAGS="-L$PREFIX/lib"
 fi
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+if [[ $(uname) == Darwin ]]; then
+	LLVMPATH = $(brew --prefix llvm)
+	echo 'export PATH="${LLVMPATH}/bin:$PATH"' >> ~/.bash_profile
+	export CC= clang
+	export CXX= $(CC)++
+	export LDFLAGS= -L${LLVMPATH}/lib -Wl,-rpath,${LLVMPATH}/lib
+	export CPPFLAGS= -I${LLVMPATH}/include -I${LLVMPATH}/include/c++/v1/
+fi
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-      -Dwith-mpi=OFF\
-      -Dwith-openmp=OFF \
-      -Dwith-python=3 \
-      -Dwith-gsl=$PREFIX \
-      -DREADLINE_ROOT_DIR=$PREFIX \
-      -DLTDL_ROOT_DIR=$PREFIX \
-      ..
+
+# Linux build
+if [[ $(uname) == Linux ]]; then
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+		  -Dwith-mpi=OFF\
+		  -Dwith-openmp=OFF \
+		  -Dwith-python=3 \
+		  -Dwith-gsl=$PREFIX \
+		  -DREADLINE_ROOT_DIR=$PREFIX \
+		  -DLTDL_ROOT_DIR=$PREFIX \
+		  ..
+endif
+
+# OSX build
+if [[ $(uname) == Darwin ]]; then
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+		  -Dwith-mpi=OFF \
+		  -Dwith-openmp=OFF \
+		  -Dwith-python=3 \
+		  -Dwith-gsl=$PREFIX \
+		  -DREADLINE_ROOT_DIR=$PREFIX \
+		  -DLTDL_ROOT_DIR=$PREFIX \
+		  ..
+fi
+
 
 make -j${CPU_COUNT}
 make install
