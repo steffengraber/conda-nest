866369bcc1509e0020130dce9837a87c1e6d68f0
Steffen Graber <s.graber@fz-juelich.de>
2019-05-09 12:27:32 +0200

    master

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-VERSION="2.16.0"
+VERSION="master"
 conda install -y -c anaconda anaconda-client
-PKG_NAME=$(conda build --output nest/ )
-anaconda -t $c_token upload -v $VERSION --force -u $c_user -l daily -l main
- $PKG_NAME
+PKG_NAME=$(conda build ./nest . --output)
+
+echo '######### UPLOAD ###########'
+echo $PKG_NAME
+
+anaconda -t $c_token upload -v $VERSION --force -u $c_user -l daily -l main $PKG_NAME
