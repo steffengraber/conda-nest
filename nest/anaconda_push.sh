10222d0636a0c44dd21b3a472919e837cedc6a91
Steffen Graber <s.graber@fz-juelich.de>
2019-05-09 15:37:39 +0200

    anaconda upload with debug information

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -6,8 +6,8 @@ PKG_NAME=$(conda build ./nest . --output)
 
 echo $PKG_NAME
 set -- $PKG_NAME
-echo '\n\n########## UPLOAD ###########'
+echo '########## UPLOAD ###########'
 echo $2
-echo '\n\n\n########## START ANACONDA CLIENT ###########'
-anaconda -v -t $c_token upload --version $VERSION --force -u $c_user -l daily -l main $2
-echo '\n\n\n########## END ANACONDA CLIENT ###########'
+echo '########## START ANACONDA CLIENT ###########'
+anaconda --verbose --show-traceback -t $c_token upload --version $VERSION --force -u $c_user -l daily -l main $2
+echo '########## END ANACONDA CLIENT ###########'
