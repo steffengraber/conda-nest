df9b04df4e3d616118b34eb69c26289e37e99410
Steffen Graber <s.graber@fz-juelich.de>
2019-05-10 06:14:01 +0200

    set python version to 3,7

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -11,3 +11,11 @@ echo $2
 echo '########## START ANACONDA CLIENT ###########'
 anaconda --verbose --show-traceback -t $c_token upload --version $VERSION --force -u $c_user -l daily -l main $2
 echo '########## END ANACONDA CLIENT ###########'
+
+
+# VERSION="v2.16.0"
+
+# PKG_NAME=$(conda build . --output)
+
+# anaconda -t $c_token upload -v $VERSION --force -u $c_user -l release -l
+# main $PKG_NAME
