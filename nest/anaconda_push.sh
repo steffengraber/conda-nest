82e03f3461102b5039efb044124315a8d4b421d3
Steffen Graber <s.graber@fz-juelich.de>
2019-05-10 06:23:42 +0200

    getting anaconda upload to work

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -6,10 +6,13 @@ PKG_NAME=$(conda build ./nest . --output)
 
 echo $PKG_NAME
 set -- $PKG_NAME
+
 echo '########## UPLOAD ###########'
-echo $2
+echo $1
 echo '########## START ANACONDA CLIENT ###########'
-anaconda --verbose --show-traceback -t $c_token upload --version $VERSION --force -u $c_user -l daily -l main $2
+
+anaconda -t $c_token upload --version ${VERSION} --force -u $c_user -l testing $2
+
 echo '########## END ANACONDA CLIENT ###########'
 
 
