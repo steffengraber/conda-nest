b7a0b179631f982b37878b09566fc06577386407
Steffen Graber <s.graber@fz-juelich.de>
2019-05-14 14:55:01 +0200

    minor

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -4,12 +4,13 @@ VERSION="master"
 conda install -y -c anaconda anaconda-client
 PKG_NAME=$(conda build ./nest . --output)
 
+echo $PKG_NAME
 set -- $PKG_NAME
 
 echo '########## UPLOAD ###########'
 echo $1
 echo '########## START ANACONDA CLIENT ###########'
 
-anaconda -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'testing, master' $1
+anaconda -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'testing' $1
 
 echo '########## END ANACONDA CLIENT ###########'
