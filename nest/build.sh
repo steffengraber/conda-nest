db657524cc876cdf80c637fab04f96edd299f0ba
Steffen Graber <s.graber@fz-juelich.de>
2019-07-23 09:08:27 +0200

    Going back

--- nest/build.sh
+++ nest/build.sh
@@ -9,11 +9,11 @@ if [[ $(uname) == Linux ]]; then
 fi
 
 if [[ $(uname) == Darwin ]]; then
-	echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
 	export CC=clang
 	export CXX=${CC}++
-	export LDFLAGS="-L${CONDA_PREFIX}/lib -Wl,-rpath,$${CONDA_PREFIX}/lib"
-	export CPPFLAGS="-I${CONDA_PREFIX}/include -I${CONDA_PREFIX}/include/c++/v1/"
+	echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
+	export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+	export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/"
 fi
 
 mkdir build
@@ -33,17 +33,16 @@ fi
 
 # OSX build
 if [[ $(uname) == Darwin ]]; then
-    echo "Start cmake now:"
-	cmake -DCMAKE_INSTALL_PREFIX:PATH=${CONDA_PREFIX}\
-	      -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" \
- 		  -Dwith-mpi=OFF \
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+		  -Dwith-mpi=OFF \
 		  -Dwith-openmp=OFF \
 		  -Dwith-python=3 \
 		  -DPYTHON_EXECUTABLE=${PYTHON}\
-		  -DPYTHON_LIBRARY=${CONDA_PREFIX}${PY_VER}.dylib \
-		  -Dwith-gsl=${CONDA_PREFIX} \
-		  -DREADLINE_ROOT_DIR=${CONDA_PREFIX} \
-		  -DLTDL_ROOT_DIR=${CONDA_PREFIX} \
+		  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
+		  -Dwith-gsl=${PREFIX} \
+		  -DREADLINE_ROOT_DIR=${PREFIX} \
+		  -DLTDL_ROOT_DIR=${PREFIX} \
 		  ..
 fi
 
