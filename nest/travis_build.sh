a1ac6287d860cab42301ae0670167941ec5a49b5
Steffen Graber <s.graber@fz-juelich.de>
2019-08-02 14:53:28 +0200

    openmp off and mpi off

--- nest/travis_build.sh
+++ nest/travis_build.sh
@@ -15,6 +15,8 @@ export CXX=clang++
 
   echo "BUILD FOR DARWIN"
 	cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+		  -Dwith-openmp=OFF \
+		  -Dwith-mpi=OFF \
 		  -Dwith-python=3 \
 		  -DCMAKE_C_COMPILER=gcc\
           -DCMAKE_CXX_COMPILER=g++\
