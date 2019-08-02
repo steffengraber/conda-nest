656c7eef2fd146106da57a1972676ebfcd58363e
Steffen Graber <s.graber@fz-juelich.de>
2019-08-02 14:20:04 +0200

    gcc and g++

--- nest/travis_build.sh
+++ nest/travis_build.sh
@@ -16,6 +16,6 @@ export CXX=clang++
   echo "BUILD FOR DARWIN"
 	cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
 		  -Dwith-python=3 \
-		  -DCMAKE_C_COMPILER=gcc-10\
-          -DCMAKE_CXX_COMPILER=g++-10 \
+		  -DCMAKE_C_COMPILER=gcc\
+          -DCMAKE_CXX_COMPILER=g++\
 		  ../nest-simulator
