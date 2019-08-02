ce806d9898cd4ced60125570a81188de038d3797
Steffen Graber <s.graber@fz-juelich.de>
2019-08-02 11:37:38 +0200

    try normal NEST installation on mac

---
+++ nest/travis_build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export MPI_FLAGS=--allow-run-as-root
+
+mkdir build
+mkdir install
+
+git clone  https://github.com/nest/nest-simulator.git
+
+cd build
+export CC=clang
+export CXX=clang++
+
+  echo "BUILD FOR DARWIN"
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+		  -Dwith-python=3 \
+		  -DCMAKE_C_COMPILER=gcc-8\
+          -DCMAKE_CXX_COMPILER=g++-8 \
+		  --/nest-simulator
