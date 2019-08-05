f7e120b92f707297a460a70c36f465ab14195a6e
Steffen Graber <s.graber@fz-juelich.de>
2019-08-05 15:09:34 +0200

    setting cmake stuff

--- nest/build.sh
+++ nest/build.sh
@@ -12,16 +12,22 @@ git clone  https://github.com/nest/nest-simulator.git
 ls -l
 
 cd build
-export CC=clang
-export CXX=clang++
+
+echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
+export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/"
 
 echo "BUILD FOR DARWIN"
 cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+	  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
 	  -Dwith-openmp=OFF \
 	  -Dwith-mpi=OFF \
 	  -Dwith-python=3 \
-	  -DCMAKE_C_COMPILER=gcc\
-	  -DCMAKE_CXX_COMPILER=g++\
+	  -DPYTHON_EXECUTABLE=${PYTHON}\
+	  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
+	  -Dwith-gsl=${PREFIX} \
+	  -DREADLINE_ROOT_DIR=${PREFIX} \
+	  -DLTDL_ROOT_DIR=${PREFIX} \
 	  ../nest-simulator
 make
 make install
