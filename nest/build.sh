5d3f29001c5bee19c417ec4e967fef5d85ec4e06
Steffen Graber <s.graber@fz-juelich.de>
2019-08-06 09:19:34 +0200

    mpi OFF

--- nest/build.sh
+++ nest/build.sh
@@ -20,19 +20,19 @@ fi
 mkdir build
 cd build
 
-mpi_arg=""
-if [[ "$mpi" != "nompi" ]]; then
-  mpi_arg="ON"
-else
-	mpi_arg="OFF"
-fi
-echo "Der MPI-Flag lautet: ${mpi_arg}"
+#mpi_arg=""
+#if [[ "$mpi" != "nompi" ]]; then
+#  mpi_arg="ON"
+#else
+#	mpi_arg="OFF"
+#fi
+#echo "Der MPI-Flag lautet: ${mpi_arg}"
 
 # Linux build
 if [[ $(uname) == Linux ]]; then
 	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
-		  -Dwith-mpi=${mpi_arg} \
-		  -Dwith-openmp=${mpi_arg} \
+		  -Dwith-mpi=OFF \
+		  -Dwith-openmp=OFF \
 		  -Dwith-python=3 \
 		  -Dwith-gsl=${PREFIX} \
 		  -DREADLINE_ROOT_DIR=${PREFIX} \
@@ -44,8 +44,8 @@ fi
 if [[ $(uname) == Darwin ]]; then
 	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
 		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
-		  -Dwith-mpi=${mpi_arg} \
-		  -Dwith-openmp=${mpi_arg} \
+		  -Dwith-mpi=OFF \
+		  -Dwith-openmp=OFF \
 		  -Dwith-python=3 \
 		  -DPYTHON_EXECUTABLE=${PYTHON}\
 		  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
