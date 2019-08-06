c8daa397cffd3123e60676199aae9dcd6a989794
Steffen Graber <s.graber@fz-juelich.de>
2019-08-06 06:45:49 +0200

    set compilers in cmake

--- nest/build.sh
+++ nest/build.sh
@@ -2,6 +2,18 @@
 
 #!/bin/sh
 
+
+# activate the build environment
+. activate "${BUILD_PREFIX}"
+
+# "stack" the host environment on top of the build env
+mkdir -p "${PREFIX}/conda-meta"
+touch "${PREFIX}/conda-meta/history"
+unset CONDA_PATH_BACKUP
+export CONDA_MAX_SHLVL=2
+source ${BUILD_PREFIX}/bin/activate "${PREFIX}"
+
+
 export MPI_FLAGS=--allow-run-as-root
 
 mkdir build
@@ -19,7 +31,7 @@ export CXXFLAGS="${CFLAGS}
 
 echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
 export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/"
+export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/ -fopenmp"
 
 
 echo "BUILD FOR DARWIN"
@@ -33,6 +45,8 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
 	  -Dwith-gsl=${PREFIX} \
 	  -DREADLINE_ROOT_DIR=${PREFIX} \
 	  -DLTDL_ROOT_DIR=${PREFIX} \
+	  -DCMAKE_C_COMPILER=${PREFIX}/bin/clang \
+	  -DCMAKE_CXX_COMPILER=${PREFIX}/bin/clang++ \
 	  ../nest-simulator
 make
 make install
