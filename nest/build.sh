980570cae6be7591e52f8a0b79805fadcc12bf5a
Steffen Graber <s.graber@fz-juelich.de>
2019-09-05 15:33:12 +0200

    skip mac build

--- nest/build.sh
+++ nest/build.sh
@@ -25,29 +25,47 @@ ls -l
 
 cd build
 
-export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-export CXXFLAGS="${CFLAGS}
+if [[ "$(uname)" == "Darwin" ]]; then
+
+	export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+	export CXXFLAGS="${CFLAGS}
 
 
-echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
-export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
-export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/ -fopenmp"
+	echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
+	export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+	export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/ -fopenmp"
 
 
-echo "BUILD FOR DARWIN"
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
-	  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
-	  -Dwith-openmp=OFF \
-	  -Dwith-mpi=OFF \
-	  -Dwith-python=3 \
-	  -DPYTHON_EXECUTABLE=${PYTHON}\
-	  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
-	  -Dwith-gsl=${PREFIX} \
-	  -DREADLINE_ROOT_DIR=${PREFIX} \
-	  -DLTDL_ROOT_DIR=${PREFIX} \
-	  -DCMAKE_C_COMPILER=${PREFIX}/bin/clang \
-	  -DCMAKE_CXX_COMPILER=${PREFIX}/bin/clang++ \
-	  ../nest-simulator
+	echo "BUILD FOR DARWIN"
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+		  -Dwith-openmp=OFF \
+		  -Dwith-mpi=OFF \
+		  -Dwith-python=3 \
+		  -DPYTHON_EXECUTABLE=${PYTHON}\
+		  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
+		  -Dwith-gsl=${PREFIX} \
+		  -DREADLINE_ROOT_DIR=${PREFIX} \
+		  -DLTDL_ROOT_DIR=${PREFIX} \
+		  -DCMAKE_C_COMPILER=${PREFIX}/bin/clang \
+		  -DCMAKE_CXX_COMPILER=${PREFIX}/bin/clang++ \
+		  ../nest-simulator
+
+else
+
+	export CC=clang
+	export CXX=clang++
+
+	echo "BUILD FOR DARWIN"
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+		  -Dwith-openmp=OFF \
+		  -Dwith-mpi=OFF \
+		  -Dwith-python=3 \
+		  -DCMAKE_C_COMPILER=gcc\
+		  -DCMAKE_CXX_COMPILER=g++\
+		  ../nest-simulator
+fi
+
 make
 make install
 cd ../install
