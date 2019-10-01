7df71b388e48cd338bc82ff919e14abf81cf0762
Steffen Graber <s.graber@fz-juelich.de>
2019-10-01 08:07:53 +0200

    shortened

--- nest/build.sh
+++ nest/build.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-export MPI_FLAGS=--allow-run-as-root
-
-mkdir build
-mkdir install
-
-git clone  https://github.com/nest/nest-simulator.git
+echo "Start Script"
 
 ls -l
 
+mkdir build
+mkdir install
 cd build
-
-echo "BUILD FOR DARWIN"
 cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
-	  -Dwith-openmp=OFF \
-	  -Dwith-mpi=OFF \
-	  -Dwith-python=3 \
 	  -DCMAKE_C_COMPILER=gcc\
 	  -DCMAKE_CXX_COMPILER=g++\
 	  ../nest-simulator
 make
 make install
-cd ../install
+
+# export MPI_FLAGS=--allow-run-as-root
+
+#mkdir build
+#mkdir install
+
+#git clone  https://github.com/nest/nest-simulator.git
+
+#ls -l
+
+#cd build
+
+#echo "BUILD FOR DARWIN"
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=../install \
+#	  -Dwith-openmp=OFF \
+#	  -Dwith-mpi=OFF \
+#	  -Dwith-python=3 \
+##	  -DCMAKE_C_COMPILER=gcc\
+#	  -DCMAKE_CXX_COMPILER=g++\
+#	  ../nest-simulator
+#make
+#make install
+#cd ../install
 
