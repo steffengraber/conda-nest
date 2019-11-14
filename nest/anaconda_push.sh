8ad062b31a10e0a80ffdf7fa4ab120fac2abd25b
graber <s.graber@fz-juelich.de>
2019-11-14 14:11:24 +0100

    Build ubuntu and macos packages

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 VERSION="master"
+
+echo "CREATE ENV UPLOAD"
+conda create --name upload
+conda activate upload
 conda install -y -c anaconda anaconda-client
 PKG_NAME=$(conda build ./nest . --output)
 
+echo 'PAKETNAME'
 echo $PKG_NAME
 set -- $PKG_NAME
 
@@ -11,6 +16,6 @@ echo '########## UPLOAD ###########'
 echo $1
 echo '########## START ANACONDA CLIENT ###########'
 
-anaconda -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'test' $1
+anaconda -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'macos' $1
 
 echo '########## END ANACONDA CLIENT ###########'
--- nest/build.sh
+++ nest/build.sh
@@ -2,24 +2,86 @@
 
 export MPI_FLAGS=--allow-run-as-root
 
-if [ $(uname) == Linux ]; then
+
+if [[ $(uname) == Linux ]]; then
     export MPI_FLAGS="$MPI_FLAGS;-mca;plm;isolated"
+	export CFLAGS="-I${PREFIX}/include"
+	export LDFLAGS="-L${PREFIX}/lib"
 fi
 
-export CFLAGS="-I$PREFIX/include"
-export LDFLAGS="-L$PREFIX/lib"
+if [[ ${target_platform} == osx-64 ]]; then
+  CC=$(basename "${CC}")
+else
+  CC=$(basename "${GCC}")
+fi
+
+if [[ $(uname) == Darwin ]] && [[ -n ${CONDA_BUILD_SYSROOT} ]]; then
+	echo 'export ${PREFIX}/bin:$PATH"' >> ~/.bash_profile
+	CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${CFLAGS}
+    LDFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${LDFLAGS}
+    CPPFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} "${CPPFLAGS}
+	# export LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+	# export CPPFLAGS="-I${PREFIX}/include -I${PREFIX}/include/c++/v1/"
+	# Using Travis standard gcc and g++
+	# export CC=$(ls /usr/local/bin/gcc-* | grep '^/usr/local/bin/gcc-\d$')
+    # export CXX=$(ls /usr/local/bin/g++-* | grep '^/usr/local/bin/g++-\d$')
+fi
+
+CPPFLAGS=${CPPFLAGS}" -I${PREFIX}/include"
+
+export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
-      -Dwith-mpi=OFF \
-      -Dwith-openmp=ON \
-      -Dwith-python=3 \
-      -Dwith-gsl=$PREFIX \
-      -DREADLINE_ROOT_DIR=$PREFIX \
-      -DLTDL_ROOT_DIR=$PREFIX \
-      ..
-make -j2
+
+mpi_arg=""
+if [[ "$mpi" != "nompi" ]]; then
+  mpi_arg="ON"
+else
+	mpi_arg="OFF"
+fi
+echo "Der MPI-Flag lautet: ${mpi_arg}"
+
+# Linux build
+if [[ $(uname) == Linux ]]; then
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+		  -Dwith-mpi=${mpi_arg} \
+		  -Dwith-openmp=${mpi_arg} \
+		  -Dwith-python=3 \
+		  -Dwith-gsl=${PREFIX} \
+		  -DREADLINE_ROOT_DIR=${PREFIX} \
+		  -DLTDL_ROOT_DIR=${PREFIX} \
+		  ..
+fi
+
+# OSX build
+if [[ $(uname) == Darwin ]]; then
+	cmake -DCMAKE_INSTALL_PREFIX:PATH=${PREFIX} \
+		  -DCMAKE_OSX_SYSROOT=${CONDA_BUILD_SYSROOT} \
+		  -Dwith-mpi=OFF \
+		  -Dwith-openmp=OFF \
+		  -Dwith-python=3 \
+		  -DPYTHON_EXECUTABLE=${PYTHON}\
+		  -DPYTHON_LIBRARY=${PREFIX}/lib/libpython${PY_VER}.dylib \
+		  -Dwith-gsl=${PREFIX} \
+		  -DREADLINE_ROOT_DIR=${PREFIX} \
+		  -DLTDL_ROOT_DIR=${PREFIX} \
+		  ..
+fi
+
+
+make
 make install
-# this is needed to make the python bindings work
-# cp $PREFIX/lib64/* $PREFIX/lib -r
+
+if [[ -d ${PREFIX}/lib64 ]]
+then
+    cp -R ${PREFIX}/lib64/* ${PREFIX}/lib
+fi
+
+
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    sed "s#!!!SP_DIR!!!#${SP_DIR}#g" "${RECIPE_DIR}/${CHANGE}.sh" > "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
+
