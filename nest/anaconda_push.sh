608e07a2c273a0df7dbf9ac4ec66bd6be8e25c18
Steffen Graber <s.graber@fz-juelich.de>
2019-11-15 07:53:33 +0100

    different anaconda

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -7,8 +7,18 @@ conda install -y -c anaconda anaconda-client
 PKG_NAME=$(conda build ./nest . --output)
 
 echo 'ANACONDA INSTALLATION'
-CONDA_BIN=$(which anaconda)
-echo $CONDA_BIN
+
+if [[ $(uname) == Darwin ]]; then
+  CONDA_BIN=$(which anaconda)
+  echo "Linux"
+  echo $CONDA_BIN
+fi
+
+if [[ $(uname) == Linux ]]; then
+  CONDA_BIN=/usr/share/miniconda/bin/anaconda
+  echo "MaOsx"
+  echo $CONDA_BIN
+fi
 
 echo 'PAKETNAME'
 echo $PKG_NAME
@@ -18,7 +28,7 @@ echo '########## UPLOAD ###########'
 echo $1
 echo '########## START ANACONDA CLIENT ###########'
 
-
+echo $CO2USER
 $CONDA_BIN -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'macos' $1
 
 echo '########## END ANACONDA CLIENT ###########'
