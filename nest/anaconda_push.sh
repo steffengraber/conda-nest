6ef8eed2d32981d6a2e3a558a4104877148b4aeb
Steffen Graber <s.graber@fz-juelich.de>
2019-11-15 07:05:23 +0100

    anaconda full path

--- nest/anaconda_push.sh
+++ nest/anaconda_push.sh
@@ -3,8 +3,12 @@
 VERSION="master"
 
 echo "CREATE ENV UPLOAD"
-pip3 install anaconda-client
+conda install -y -c anaconda anaconda-client
 PKG_NAME=$(conda build ./nest . --output)
+
+echo 'ANACONDA INSTALLATION'
+CONDA_BIN=$(which anaconda)
+echo $CONDA_BIN
 
 echo 'PAKETNAME'
 echo $PKG_NAME
@@ -15,6 +19,6 @@ echo $1
 echo '########## START ANACONDA CLIENT ###########'
 
 
-bash anaconda -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'macos' $1
+$CONDA_BIN -t $CO2TOKEN upload --version ${VERSION} --force -u $CO2USER -l 'macos' $1
 
 echo '########## END ANACONDA CLIENT ###########'
